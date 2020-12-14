2f5559954ef2a54f48edc8c6a8a5b86ff8db7779
germanny <germanny@gmail.com>
2020-12-14 11:05:24 -0600

    add search

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -14,8 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
+const { Op } = require("sequelize");
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb) {
@@ -38,54 +39,82 @@ function asyncHandler(cb) {
 
 /* GET - Show full books listing. */
 router.get('/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll({ order: [["title", "ASC"]] });
-  const booksJSON = books.map(book => book.toJSON());
-  console.log('My books: ', booksJSON);
-  res.render("books/index", { books, title: "My Books List" });
+  const search = req.body.search ? req.body.search : '';
+  console.log('get search term: ', search);
+  const books = await Book.findAll({ order: [['title', 'ASC']] });
+  res.render('books/index', { books, title: 'My Books List', search });
 }));
-
-/* GET - Create a new book form. */
-router.get('/new', (req, res) => {
-  res.render("books/new", { book: {}, title: "New Book" });
-});
 
 /* POST - Create a book with the form. */
 router.post('/', asyncHandler(async (req, res) => {
   let book;
-  try {
-    book = await Book.create(req.body);
-    res.redirect("/books");
-  } catch (error) {
-    if (error.name === "SequelizeValidationError") { // checking the error
-      book = await Book.build(req.body); // return a non-persistent (or unsaved) model instance
-      res.render("books/new", { book, errors: error.errors, title: "New book" })
-    } else {
-      throw error; // error caught in the asyncHandler’s catch block
+  const search = req.body.search ? req.body.search : '';
+
+  // https://sequelize.org/master/manual/model-querying-basics.html#applying-where-clauses
+  if (search) {
+    console.log('post search term: ', search);
+    const books = await Book.findAll({
+      order: [['title', 'ASC']],
+      where: {
+        [Op.or]: {
+          title: {
+            [Op.like]: '%' + search + '%'
+          },
+          author: {
+            [Op.like]: '%' + search + '%'
+          },
+          genre: {
+            [Op.like]: '%' + search + '%'
+          },
+          year: {
+            [Op.like]: '%' + search + '%'
+          }
+        },
+      }
+    });
+    res.render('books/index', { books, title: 'Search Results', search });
+  } else {
+    console.log('no search term');
+    try {
+      book = await Book.create(req.body);
+      res.redirect('/books');
+    } catch (error) {
+      if (error.name === 'SequelizeValidationError') { // checking the error
+        book = await Book.build(req.body); // return a non-persistent (or unsaved) model instance
+        res.render('books/new', { book, errors: error.errors, title: 'New book' })
+      } else {
+        throw error; // error caught in the asyncHandler’s catch block
+      }
     }
   }
 }));
 
+/* GET - Create a new book form. */
+router.get('/new', (req, res) => {
+  res.render('books/new', { book: {}, title: 'New Book' });
+});
+
 /* GET - Edit book form. */
-router.get("/:id/edit", asyncHandler(async (req, res) => {
+router.get('/:id/edit', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   if (book) {
-    res.render("books/edit", { book, title: "Edit Book" });
+    res.render('books/edit', { book, title: 'Edit Book' });
   } else {
     res.sendStatus(404);
   }
 }));
 
 /* GET - Show individual book, using the edit template. */
-router.get("/:id", asyncHandler(async (req, res, next) => {
+router.get('/:id', asyncHandler(async (req, res, next) => {
   const book = await Book.findByPk(req.params.id);
   if (book) {
-    res.render("books/edit", { book, title: book.title });
+    res.render('books/edit', { book, title: book.title });
   } else {
     const err = new Error();
     err.message = (isNaN(req.params.id)) ?
-      "Sorry, this page does not exist. :("
+      'Sorry, this page does not exist. :('
       :
-      "Sorry, this book does not exist. :(";
+      'Sorry, this book does not exist. :(';
     err.status = (isNaN(req.params.id)) ? 400 : 500;
     next(err);
   }
@@ -100,21 +129,21 @@ router.post('/:id/edit', asyncHandler(async (req, res) => {
     if (book) {
       await book.update(req.body);
       console.log(req.body);
-      res.redirect("/books/");
+      res.redirect('/books/');
     } else {
       const err = new Error();
       err.message = (!isNaN(req.params.id)) ?
-        "Sorry, this book does not exist. :("
+        'Sorry, this book does not exist. :('
         :
-        "Sorry, this page does not exist. :(";
+        'Sorry, this page does not exist. :(';
       err.status = (!isNaN(req.params.id)) ? 500 : 400;
       next(err);
     }
   } catch (error) {
-    if (error.name === "SequelizeValidationError") {
+    if (error.name === 'SequelizeValidationError') {
       book = await Book.build(req.body);
       book.id = req.params.id; // make sure the correct book gets updated
-      res.render("books/edit", { book, errors: error.errors, title: "Edit Book" })
+      res.render('books/edit', { book, errors: error.errors, title: 'Edit Book' })
     } else {
       throw error;
     }
@@ -122,16 +151,16 @@ router.post('/:id/edit', asyncHandler(async (req, res) => {
 }));
 
 /* GET - Delete book form, using 'delete' template. */
-router.get("/:id/delete", asyncHandler(async (req, res) => {
+router.get('/:id/delete', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   if (book) {
-    res.render("books/delete", { book, title: "Delete Book" });
+    res.render('books/delete', { book, title: 'Delete Book' });
   } else {
     const err = new Error();
     err.message = (!isNaN(req.params.id)) ?
-      "Sorry, this book does not exist. :("
+      'Sorry, this book does not exist. :('
       :
-      "Sorry, this page does not exist. :(";
+      'Sorry, this page does not exist. :(';
     err.status = (!isNaN(req.params.id)) ? 500 : 400;
     next(err);
   }
@@ -142,13 +171,13 @@ router.post('/:id/delete', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   if (book) {
     await book.destroy();
-    res.redirect("/books");
+    res.redirect('/books');
   } else {
     const err = new Error();
     err.message = (!isNaN(req.params.id)) ?
-      "Sorry, this book does not exist. :("
+      'Sorry, this book does not exist. :('
       :
-      "Sorry, this page does not exist. :(";
+      'Sorry, this page does not exist. :(';
     err.status = (!isNaN(req.params.id)) ? 500 : 400;
     next(err);
   }
